0af18ef7ce8ea16c3ab4ef7cec80301d36616579
rawattanuja197-rgb <rawattanuja197@gmail.com>
2026-09-25 20:03:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// // var, let, const - line-by-line comaprison
+// ways of writing variables
+// var a; declare karna
+// var a = 12; declare and initialize
+// let a;
+// let a = 12;
+// const a = 12;
+// declarations and initialization
+
+ 
+                            
